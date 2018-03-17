57a01737411339e48f0a7057d77b5d449a1879b2
Chris Henry <cdhenry@gmail.com>
2018-03-17 01:25:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
     @post = Post.find(params[:id])
     erb :'posts/show'
   end
-  
+
   get '/posts/:id' do
     @post = Post.find(params[:id])
     erb :'posts/show'
